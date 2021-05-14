78a0bc5653d4955b68d7e478738cd78010c6d417
anogray <behlbhaskar97@gmail.com>
2021-05-15 02:28:42 +0530

    add link to website

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+      <h1 style={{"text-align":"center"}}><a href="https://next-deploytest.vercel.app">GoTo</a></h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
